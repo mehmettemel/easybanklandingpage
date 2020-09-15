f8615385429f10761236d2652aa265dad471e55b
mehmet temel <temel1072@hotmail.com>
2020-09-15 13:21:46 +0300

    first task is done

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body")
+const btnHamburger = document.getElementById("btnHamburger");
+const header = document.querySelector(".header")
+const overlay = document.querySelector(".overlay")
+const fadeElems = document.querySelectorAll(".has-fade")
+
+
+btnHamburger.addEventListener("click",()=>{
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll")
+    header.classList.remove("open")
+    fadeElems.forEach(element=>{
+      element.classList.remove("fade-in")
+      element.classList.add("fade-out")
+    })
+    
+    
+  }else{ //Open hamburger menu
+    body.classList.add("noscroll")
+    header.classList.add("open")
+    fadeElems.forEach(function(element){
+      element.classList.remove("fade-out")
+      element.classList.add("fade-in")
+
+    })
+    
+  }
+})
